aaf5ccc4aa689897a5d34ba4ba4715c277fafe1f
RyoJerryYu <tzl19961216@gmail.com>
2021-12-21 21:47:27 +0800

    add life cycle for clock class

--- source/playground/state.jsx
+++ source/playground/state.jsx
@@ -1,20 +1,37 @@
 'use strict';
 
 class Clock extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {date: new Date()};
-    }
-  
-    render() {
-      return (
-        <div>
-          <h1>Hello, world!</h1>
-          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
   }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
   
   ReactDOM.render(
     <Clock />,
