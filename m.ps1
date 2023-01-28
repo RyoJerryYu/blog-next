0cf4435b942c230320f1c0b7965b367904765fb2
Ryo <tzl19961216@gmail.com>
2023-01-28 12:37:44 +0800

    feat: use next dependence

--- m.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location .\source\_posts\2022-07-31-why-homogeneous
-Remove-Item -Recurse -Force .\media
-manim -ql --format gif .\_manim\scene.py
-Set-Location ..\..\..
--- manim.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location .\source\_posts\2022-06-28-graph-for-economics-1
-Remove-Item -Recurse -Force .\media
-manim -ql .\_manim\scene.py
-Set-Location ..\..\..
