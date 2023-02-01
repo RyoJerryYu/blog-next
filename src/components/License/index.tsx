a9bdc78805469816c195d4d0d9404746d02c1906
Ryo <tzl19961216@gmail.com>
2023-02-01 23:29:38 +0800

    feat: make license component styled

--- src/components/License/index.tsx
+++ src/components/License/index.tsx
@@ -6,30 +6,33 @@ type LicenseProps = {};
 export default function License({}: LicenseProps) {
   return (
     <>
-      <span>
-        <Link
-          rel="license"
-          href="http://creativecommons.org/licenses/by-nc/4.0/"
-        >
-          <Image
-            alt="Creative Commons License"
-            style={{ borderWidth: "0" }}
-            width={88}
-            height={31}
-            src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"
-          />
-        </Link>
-        <div>
+      <div className="w-96 text-gray-700">
+        <span className="!text-sm">
+          <Link
+            className="!inline-block !p-0 !m-0 align-text-bottom"
+            rel="license"
+            href="http://creativecommons.org/licenses/by-nc/4.0/"
+          >
+            <Image
+              className="!m-0 h-4 w-auto pr-1"
+              alt="Creative Commons License"
+              style={{ borderWidth: "0" }}
+              width={88}
+              height={31}
+              src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png"
+            />
+          </Link>
           This work is licensed under a{" "}
           <a
+            className="underline"
             rel="license"
             href="http://creativecommons.org/licenses/by-nc/4.0/"
           >
             Creative Commons Attribution-NonCommercial 4.0 International License
           </a>
           .
-        </div>
-      </span>
+        </span>
+      </div>
     </>
   );
 }
