13a50c051276adf7bebba1edd914935b6e8a3e07
Ryo <tzl19961216@gmail.com>
2023-02-03 00:22:00 +0800

    feat: add path prefix for gh-page and add gh action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: `/blog-next`,
+  basePath: `/blog-next`,
   images: {
     unoptimized: true,
     remotePatterns: [
