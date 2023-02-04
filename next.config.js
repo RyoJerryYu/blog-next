ca57e64fc4715e5a1585943f217d1794eab0e00c
Ryo <tzl19961216@gmail.com>
2023-02-05 00:47:16 +0800

    feat: boost build-time meta load with setting single thread

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   reactStrictMode: true,
   assetPrefix: `${basePath}`,
   basePath: `${basePath}`,
+  experimental: {
+    // we load metadata when build-time, which is very very slow.
+    // and those metadata are immutable and could be cached.
+    // however, caching is not available crossing different threads,
+    // so we just use single thread to build,
+    // and it's faster than using multiple threads (because of cache).
+    cpus: 1,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
