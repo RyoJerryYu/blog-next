064c4e75b51b56f693ca3392b6d52e9b5afd831e
Ryo <tzl19961216@gmail.com>
2023-02-11 01:54:34 +0800

    feat: remove trailing slash after deploying lambda

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const basePath = process.env.NEXT_PUBLIC_SITE_BASE_PATH || undefined;
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: false,
   assetPrefix: basePath,
   basePath: basePath,
   experimental: {
