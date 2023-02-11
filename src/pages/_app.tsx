c3ec04c6c1e2eb5183ceeebd60526c9ebea671ee
Ryo <tzl19961216@gmail.com>
2023-02-11 22:03:03 +0800

    feat: add google analytics for site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,23 @@ import "@/styles/globals.scss";
 import { MDXProvider } from "@mdx-js/react";
 import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <UniversalHead />
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-1Y0XYKX8HY"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1Y0XYKX8HY');`}
+      </Script>
       <MDXProvider components={components}>
         <Component {...pageProps} />
       </MDXProvider>
