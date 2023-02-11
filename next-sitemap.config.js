65f7f19ddeb7049dc9f5bb3c477a367ca600ee19
Ryo <tzl19961216@gmail.com>
2023-02-12 01:31:04 +0800

    feat: use directly site map instead of index

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next-sitemap').IConfig} */
 const siteOrigin = process.env.NEXT_PUBLIC_SITE_ORIGIN || 'https://blog.ryojerryyu.xyz';
 const basePath = process.env.NEXT_PUBLIC_SITE_BASE_PATH || "";
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: `${siteOrigin}${basePath}`,
   generateRobotsTxt: true, // (optional)
   outDir: "./out",
   // sitemapSize: 7000,
+  generateIndexSitemap: false,
 }
