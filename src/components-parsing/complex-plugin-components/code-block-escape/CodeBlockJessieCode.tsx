d27ceac3cd66865f342d81175d3f9faf462ee158
RyoJerryYu <tzl19961216@gmail.com>
2025-03-19 16:52:04 +0800

    refactor: simplify regex for parsing code block content in CodeBlockJessieCode component

--- src/components-parsing/complex-plugin-components/code-block-escape/CodeBlockJessieCode.tsx
+++ src/components-parsing/complex-plugin-components/code-block-escape/CodeBlockJessieCode.tsx
@@ -5,19 +5,15 @@ import matter from "gray-matter";
 /**
  * source 如下：
  *
+ * ---
  * boundingbox: [-10, 10, 10, -10]
  * grid: true
  * axis: true
- *
  * ---
  *
  * A = point(0, 0)
  *
- * 找到 --- 之前的内容，并将其作为 graphInfo, 之后的代码作为 codeContent
- * 分割线可能有 3 个以上的 ·-· ，之前与之后都可能有零个或多个空行
- * 分割线也可能不存在，此时整个 source 作为 codeContent , graphInfo 为默认值
  */
-const regex = /^(.*?)(?:\n*---+\n*)([\s\S]*)$/m;
 
 export const CodeBlockJessieCode = (props: CodeBlockProps) => {
   const frontMatter = matter(props.value);
