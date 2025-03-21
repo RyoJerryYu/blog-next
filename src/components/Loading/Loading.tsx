8c6290ad3376b75c795ead3e1207fd17e823298b
RyoJerryYu <tzl19961216@gmail.com>
2025-03-21 11:52:13 +0800

    feat: enhance Loading component with CircularProgress for better user feedback

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,9 @@
+import { CircularProgress } from "@mui/material";
+
 export function Loading() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
-      Loading...
+    <div className="absolute inset-0 flex items-center justify-center bg-bg-focus2">
+      <CircularProgress />
     </div>
   );
 }
