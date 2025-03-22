25edcb33428fdbf85493098ac03d92ac82665885
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2025-03-22 14:57:05 +0800

    feat: add dropdown menu functionality to DefaultLayout, enhancing navigation with new DropdownLink and DropdownMenu components

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,9 +4,20 @@ import {
   PixivIcon,
   TwitterIcon,
 } from "@/components/svgs";
-import { AppBar, Box, Slide, Toolbar, useScrollTrigger } from "@mui/material";
+import { KeyboardArrowRight } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Menu,
+  MenuItem,
+  PopoverOrigin,
+  Slide,
+  Toolbar,
+  useScrollTrigger,
+} from "@mui/material";
+import clsx from "clsx";
 import Link from "next/link";
-import React, { JSX } from "react";
+import React, { JSX, useState } from "react";
 import style from "./DefaultLayout.module.scss";
 import MainWidth from "./MainWidth";
 
@@ -25,9 +36,115 @@ type ClickableItem = {
   text: string;
 };
 type ClickableIcon = ClickableItem & { Icon: (props: IconItem) => JSX.Element };
+type ClickableMenu = ClickableItem & {
+  children?: ClickableItem[];
+};
+
+////////
+// Header Components
+////////
+
+function TextLink(props: ClickableItem) {
+  return (
+    <Link href={props.href} className={style.textlink}>
+      {props.text}
+    </Link>
+  );
+}
+
+function IconLink(props: ClickableIcon) {
+  return (
+    <Link href={props.href} title={props.text} key={props.text}>
+      <props.Icon className="h-6 w-6 fill-gray-300 hover:fill-white transition-all ease-in-out mx-1 md:mx-2" />
+    </Link>
+  );
+}
+
+type DropdownState = {
+  anchorEl: HTMLElement | null;
+  isOpen: boolean;
+  openMenu: (event: React.MouseEvent<HTMLElement>) => void;
+  closeMenu: () => void;
+};
+
+function useDropdownState() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isOpen = Boolean(anchorEl);
+
+  const openMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return { anchorEl, isOpen, openMenu, closeMenu };
+}
+
+type DropdownMenuProps = {
+  id: string;
+  items: ClickableItem[];
+  anchorOrigin: PopoverOrigin;
+  transformOrigin: PopoverOrigin;
+  dropdownState: DropdownState;
+};
+function DropdownMenu(props: DropdownMenuProps) {
+  return (
+    <Menu
+      id={props.id}
+      anchorEl={props.dropdownState.anchorEl}
+      anchorOrigin={props.anchorOrigin}
+      transformOrigin={props.transformOrigin}
+      open={props.dropdownState.isOpen}
+      onClose={props.dropdownState.closeMenu}
+      disableAutoFocusItem
+      disableRestoreFocus
+    >
+      {props.items.map((item) => (
+        <MenuItem key={item.text} onClick={props.dropdownState.closeMenu}>
+          <Link href={item.href} className={style.dropdownitem}>
+            {item.text}
+          </Link>
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+}
+
+function DropdownLink(props: ClickableMenu) {
+  const dropdownState = useDropdownState();
+
+  return (
+    <>
+      <Box onClick={dropdownState.openMenu} className={style.textlink}>
+        {props.text}
+        <KeyboardArrowRight
+          className={clsx(dropdownState.isOpen && "rotate-90")}
+        />
+      </Box>
+      <DropdownMenu
+        id={`${props.text}-menu`}
+        items={props.children ?? []}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        transformOrigin={{ vertical: "top", horizontal: "left" }}
+        dropdownState={dropdownState}
+      />
+    </>
+  );
+}
+
+function MenuBar(props: { items: ClickableMenu[] }) {
+  return props.items.map((item) => (
+    <div key={item.text} className="relative float-left mx-1 md:mx-2">
+      {item.children ? <DropdownLink {...item} /> : <TextLink {...item} />}
+    </div>
+  ));
+}
+
 type DefaultHeaderProps = {
   homeItem: ClickableItem;
-  menuItems: ClickableItem[];
+  menuItems: ClickableMenu[];
   iconItems: ClickableIcon[];
 };
 const DefaultHeader: React.FC<DefaultHeaderProps> = ({
@@ -42,23 +159,12 @@ const DefaultHeader: React.FC<DefaultHeaderProps> = ({
           <Toolbar>
             {/* home */}
             <div className="ml-2 w-32 mr-2 md:mr-4">
-              <Link href={homeItem.href} className={style.textlink}>
-                {homeItem.text}
-              </Link>
+              <TextLink {...homeItem} />
             </div>
 
             {/* menu */}
             <Box sx={{ display: "flex" }}>
-              {menuItems.map((item) => (
-                <div
-                  key={item.text}
-                  className="relative float-left mx-1 md:mx-2"
-                >
-                  <Link href={item.href} className={style.textlink}>
-                    {item.text}
-                  </Link>
-                </div>
-              ))}
+              <MenuBar items={menuItems} />
             </Box>
 
             <Box sx={{ flexGrow: 1, display: "flex" }}></Box>
@@ -66,9 +172,7 @@ const DefaultHeader: React.FC<DefaultHeaderProps> = ({
             {/* icon */}
             <Box sx={{ display: "flex" }}>
               {iconItems.map((item) => (
-                <Link href={item.href} title={item.text} key={item.text}>
-                  <item.Icon className="h-6 w-6 fill-gray-300 hover:fill-white transition-all ease-in-out mx-1 md:mx-2" />
-                </Link>
+                <IconLink key={item.text} {...item} />
               ))}
             </Box>
           </Toolbar>
@@ -119,12 +223,19 @@ type DefaultLayoutProps = {
 );
 const DefaultLayout: React.FC<DefaultLayoutProps> = (props) => {
   const homeItem: ClickableItem = { href: "/", text: "Ryo's Blog" };
-  const menuItems: ClickableItem[] = [
+  const menuItems: ClickableMenu[] = [
     { href: "/articles", text: "Articles" },
     { href: "/ideas", text: "Ideas" },
     { href: "/learn_from_ai", text: "Learn from AI" },
     { href: "/tags", text: "Tags" },
-    { href: "/clips", text: "Clips" },
+    {
+      href: "",
+      text: "More",
+      children: [
+        { href: "/clips", text: "Clips" },
+        { href: "/prev", text: "Preview" },
+      ],
+    },
   ];
   const iconItems: ClickableIcon[] = [
     {
