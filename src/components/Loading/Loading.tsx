f173b9a4f03308b05217ec37405a1b78c9ddf99c
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2025-03-24 00:30:34 +0800

    fix: adjust Loading component layout for full-width and height display

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import { CircularProgress } from "@mui/material";
 
 export function Loading() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-bg-focus2">
+    <div className="inset-0 w-full h-full flex items-center justify-center bg-bg-focus2">
       <CircularProgress />
     </div>
   );
