d046bb44f6cb0033e1666ec1432d244384a7f03e
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2025-04-30 00:03:38 +0800

    feat: implement wiki page template for dynamic rendering

--- src/pages/testwiki/[[...slugs]].tsx
+++ src/pages/testwiki/[[...slugs]].tsx
@@ -1,17 +1,11 @@
-import { ParsingProvider } from "@/components-parsing/component-parsing";
-import { Anchor } from "@/components/antd/Anchor";
-import BackRefList from "@/components/BackRefList/BackRefList";
-import Post from "@/components/Post";
-import { WikiTreeMenu } from "@/components/wiki/WikiTreeMenu";
 import { loadCache } from "@/core/indexing/indexing-cache";
 import { testwikiPathMapper } from "@/core/indexing/indexing-settings";
+import { buildWikiPage } from "@/core/page-template/wiki-page";
 import {
   wikiGetStaticPaths,
   wikiGetStaticProps,
 } from "@/core/page-template/wiki-static";
 import { WikiPageProps } from "@/core/page-template/wiki-type";
-import DefaultLayout from "@/layouts/DefaultLayout";
-import { Description, Title } from "@/layouts/UniversalHead";
 import { GetStaticPaths, GetStaticProps } from "next";
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -29,38 +23,6 @@ export const getStaticProps: GetStaticProps<
   return await wikiGetStaticProps("testwiki", slugs, pathMapper);
 };
 
-const TestWikiPage = (props: WikiPageProps) => {
-  return (
-    <>
-      <Title>{props.meta.title}</Title>
-      <Description>{props.meta.abstract}</Description>
-      <DefaultLayout
-        left={
-          <WikiTreeMenu
-            trees={props.wikiTree.trees}
-            currentSlugs={props.slugs}
-          />
-        }
-        right={
-          <Anchor
-            items={props.meta.headingTrees}
-            offsetTop={64}
-            className="overflow-y-auto"
-          />
-        }
-      >
-        <ParsingProvider>
-          <Post
-            meta={props.meta}
-            tags={props.tags}
-            source={props.source}
-            prevNextInfo={{ prevInfo: null, nextInfo: null }}
-          />
-        </ParsingProvider>
-        <BackRefList posts={props.backRefResources} />
-      </DefaultLayout>
-    </>
-  );
-};
+const TestWikiPage = buildWikiPage();
 
 export default TestWikiPage;
