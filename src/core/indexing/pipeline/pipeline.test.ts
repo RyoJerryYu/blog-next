0de0ecc1a8e52319a0ca4e9f994c2ac67bf8f97f
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2025-05-02 23:42:23 +0800

    test: fix type error for pipeline test

--- src/core/indexing/pipeline/pipeline.test.ts
+++ src/core/indexing/pipeline/pipeline.test.ts
@@ -1,10 +1,10 @@
 import { testNoCI } from "@/utils/utils.test";
 import { pipeline } from "../indexing-settings";
-import { executePipeline } from "./pipeline";
+import { executePipeline, ResourcePoolFromScratch } from "./pipeline";
 
 testNoCI("execute pipeline", async () => {
-  const p = pipeline;
-  const res = await executePipeline(p);
+  const p = pipeline();
+  const res = await executePipeline(p, new ResourcePoolFromScratch());
   const indexKeys = Object.keys(res.indexPool);
   expect(indexKeys.length).toBeGreaterThan(0);
   indexKeys.forEach((key) => {
