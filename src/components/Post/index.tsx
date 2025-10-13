3b0e51ea8051e4c06ee335077f73ff5bd1fa0f27
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2025-10-14 01:46:52 +0800

    refactor(Post): remove unused styles and simplify component classes for improved readability

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,13 +1,11 @@
 import { PrevNextInfo } from "@/core/indexing/index-building/prev-next-index-builder/types";
 import { TagInfo } from "@/core/indexing/index-building/tag-index-builder/types";
 import { PostMeta } from "@/core/types/indexing";
-import clsx from "clsx";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import Link from "next/link";
 import License from "../License";
 import RelativeTime from "../RelativeTime";
 import TagsBox from "../TagsBox";
-import style from "./Post.module.scss";
 
 type PostProps = {
   source: MDXRemoteSerializeResult;
@@ -22,13 +20,13 @@ const Post = ({ meta, source, tags, prevNextInfo }: PostProps) => {
     <article className="post-frame">
       <h1>{meta.title}</h1>
       {meta.created_at && (
-        <RelativeTime className={style.postDate}>
+        <RelativeTime className="text-sm text-slate-700">
           {meta.created_at}
         </RelativeTime>
       )}
       {meta.tags.length > 0 && <TagsBox className="mt-2" tags={tags} />}
 
-      <div className={clsx("post-body", style.postContent)}>
+      <div className="post-body">
         <MDXRemote {...source} />
       </div>
       {meta.license && <License />}
