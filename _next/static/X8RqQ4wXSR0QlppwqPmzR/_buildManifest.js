d604028de70642c5feaa1f7dd7c16716f7a9c0df
RyoJerryYu <RyoJerryYu@users.noreply.github.com>
2026-01-22 18:13:59 +0000

    Deploying to gh-pages from @ RyoJerryYu/blog-next@36d253659e42a92b5a770e93798f742a98df5a04 🚀

---
+++ _next/static/X8RqQ4wXSR0QlppwqPmzR/_buildManifest.js
@@ -0,0 +1,70 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/da4cc4882352aafa.js"
+  ],
+  "/_error": [
+    "static/chunks/8996f53abfab913e.js"
+  ],
+  "/articles": [
+    "static/chunks/13a49eb095f55042.js"
+  ],
+  "/articles/[slug]": [
+    "static/chunks/72f5059d26df1411.js"
+  ],
+  "/clips": [
+    "static/chunks/699fb477c7ceee24.js"
+  ],
+  "/ideas": [
+    "static/chunks/c0433c1168ae2dc9.js"
+  ],
+  "/ideas/[slug]": [
+    "static/chunks/2fdfb5410271baa2.js"
+  ],
+  "/jessiecode-wiki/[[...slugs]]": [
+    "static/chunks/c17ba57bc1c662d1.js"
+  ],
+  "/learn_from_ai": [
+    "static/chunks/c8326d783b9018cf.js"
+  ],
+  "/learn_from_ai/[slug]": [
+    "static/chunks/62609550162d6a1b.js"
+  ],
+  "/prev": [
+    "static/chunks/c4070ad47bb8a2c7.js"
+  ],
+  "/tags": [
+    "static/chunks/b2553bbc02a8b72a.js"
+  ],
+  "/tags/[tag]": [
+    "static/chunks/66c35e5fae08ab5e.js"
+  ],
+  "/testwiki/[[...slugs]]": [
+    "static/chunks/a8f8b22eb57b8c97.js"
+  ],
+  "/ygowiki/[[...slugs]]": [
+    "static/chunks/6735ebe4e625c74f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/articles",
+    "/articles/[slug]",
+    "/clips",
+    "/ideas",
+    "/ideas/[slug]",
+    "/jessiecode-wiki/[[...slugs]]",
+    "/learn_from_ai",
+    "/learn_from_ai/[slug]",
+    "/prev",
+    "/tags",
+    "/tags/[tag]",
+    "/testwiki/[[...slugs]]",
+    "/ygowiki/[[...slugs]]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
