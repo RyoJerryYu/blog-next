fb9b5a4238990dc6e34e0bf93eb17afc7f79a91e
RyoJerryYu <RyoJerryYu@users.noreply.github.com>
2026-01-23 15:38:57 +0000

    Deploying to gh-pages from @ RyoJerryYu/blog-next@9406d0e927024676fb6afb97e30880a234bfb467 🚀

---
+++ _next/static/chunks/1569c8309758eea8.js
@@ -0,0 +1,15 @@
+__turbopack_load_page_chunks__("/ideas", [
+  "static/chunks/e8571701c086ade6.js",
+  "static/chunks/97dc5ca197e3bddf.js",
+  "static/chunks/0207c8977e5b26f5.js",
+  "static/chunks/12d481071a046fa2.js",
+  "static/chunks/4063dd56671a94ef.js",
+  "static/chunks/2ab937bd5885f520.js",
+  "static/chunks/83ee798490185082.js",
+  "static/chunks/b22399fb570c4f92.js",
+  "static/chunks/0fa13ce69ac79c95.js",
+  "static/chunks/37df600a63f0b777.js",
+  "static/chunks/b5cbeb4775d0f556.css",
+  "static/chunks/9f00b5a374176fb7.css",
+  "static/chunks/turbopack-3fd6185a57205251.js"
+])
---
+++ _next/static/chunks/58adcbe3ac61747c.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/clips", [
+  "static/chunks/a30990cc3b639775.js",
+  "static/chunks/97dc5ca197e3bddf.js",
+  "static/chunks/18d1c43858ec8e25.js",
+  "static/chunks/0207c8977e5b26f5.js",
+  "static/chunks/12d481071a046fa2.js",
+  "static/chunks/f8e8de3e7543574e.js",
+  "static/chunks/83ee798490185082.js",
+  "static/chunks/b22399fb570c4f92.js",
+  "static/chunks/b5cbeb4775d0f556.css",
+  "static/chunks/9f00b5a374176fb7.css",
+  "static/chunks/turbopack-c0db4dcc936eb66b.js"
+])
