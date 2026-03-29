84ec5736c8c916610cdaeded4cbbcf67efb31d3e
RyoJerryYu <RyoJerryYu@users.noreply.github.com>
2026-03-29 15:40:32 +0000

    Deploying to gh-pages from @ RyoJerryYu/blog-next@ca2ab1689a418d35f9d5ffe2bf17e5eb9931b602 🚀

---
+++ _next/static/chunks/0usfc16pukbtq.js
@@ -0,0 +1,16 @@
+__turbopack_load_page_chunks__("/ygowiki/[[...slugs]]", [
+  "static/chunks/0snnbkr~e7_ut.js",
+  "static/chunks/125hi0_74.j2c.js",
+  "static/chunks/16m1f3vsub1ht.js",
+  "static/chunks/0f237uxdvt8i3.js",
+  "static/chunks/00rwt3pwgp30i.js",
+  "static/chunks/10yw01wtv9.0~.js",
+  "static/chunks/00a0i9v-zb_64.js",
+  "static/chunks/0reeii0tlaee0.js",
+  "static/chunks/0a.p798in.30-.js",
+  "static/chunks/02g.-k-xryis~.js",
+  "static/chunks/0_r2knpnc0spf.js",
+  "static/chunks/07bd~.h5tw7dt.css",
+  "static/chunks/10oo6.5m3nmlt.css",
+  "static/chunks/turbopack-0g3xsbh2cxu1s.js"
+])
